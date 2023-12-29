6fca83db5edfde96475a131c1c9c33b2238f4e8e
pasinduQB <107556360+pasinduQB@users.noreply.github.com>
2023-12-29 13:04:33 +0530

    fix: tab update

--- src/components/Tabs/theme.ts
+++ src/components/Tabs/theme.ts
@@ -5,41 +5,41 @@ export const tabTheme: FlowbiteTabsTheme = {
   tablist: {
     base: 'flex text-center',
     styles: {
-      default: 'flex-wrap border-b border-themeGray-80 dark:border-gray-700',
-      underline: 'flex-wrap -mb-px border-b border-themeGray-80 dark:border-gray-700',
+      default: 'flex-wrap dark:border-gray-700',
+      underline: 'flex-wrap -mb-px dark:border-gray-700',
       pills: 'flex-wrap font-medium text-sm text-gray-500 dark:text-gray-400 space-x-2',
       fullWidth:
-        'w-full text-sm font-medium divide-x divide-themeGray-80 shadow grid grid-flow-col dark:divide-gray-700 dark:text-gray-400 rounded-none',
+        'w-full text-sm font-medium divide-x divide-themeGray-80 grid grid-flow-col dark:divide-gray-700 dark:text-gray-400 rounded-none',
     },
     tabitem: {
-      base: 'flex items-center justify-center p-4 rounded-t-lg text-sm font-medium first:ml-0 disabled:cursor-not-allowed disabled:text-gray-400 disabled:dark:text-gray-500 focus:ring-4 focus:ring-cyan-300 focus:outline-none',
+      base: 'flex items-center justify-center p-4 rounded-t-lg text-sm font-medium first:ml-0 disabled:cursor-not-allowed disabled:text-gray-400 disabled:dark:text-gray-500 focus:outline-none',
       styles: {
         default: {
           base: 'rounded-t-lg',
           active: {
-            on: 'bg-gray-100 text-cyan-600 dark:bg-gray-800 dark:text-cyan-500',
-            off: 'text-gray-500 hover:bg-gray-50 hover:text-gray-600 dark:text-gray-400 dark:hover:bg-gray-800  dark:hover:text-gray-300',
+            on: 'bg-themeGray-70 text-primary-100 font-bold dark:bg-gray-800 dark:text-cyan-500',
+            off: 'text-themeGray-100 hover:bg-themeGray-60 hover:text-themeGray-200 dark:text-gray-400 dark:hover:bg-gray-800  dark:hover:text-gray-300',
           },
         },
         underline: {
           base: 'rounded-t-lg',
           active: {
-            on: 'text-cyan-600 rounded-t-lg border-b-2 border-cyan-600 active dark:text-cyan-500 dark:border-cyan-500',
-            off: 'border-b-2 border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300',
+            on: 'text-primary-100 font-bold rounded-t-lg border-b-4 border-primary-100 active dark:text-cyan-500 dark:border-cyan-500',
+            off: 'border-b-4 border-transparent text-themeGray-100 hover:border-themeGray-70 hover:text-themeGray-200 dark:text-gray-400 dark:hover:text-gray-300',
           },
         },
         pills: {
           base: '',
           active: {
-            on: 'rounded-lg bg-cyan-600 text-white',
-            off: 'rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white',
+            on: 'rounded-lg bg-secondary-100 text-primary-100 font-bold',
+            off: 'rounded-lg hover:text-themeGray-200 hover:bg-themeGray-60 dark:hover:bg-gray-800 dark:hover:text-white',
           },
         },
         fullWidth: {
           base: 'ml-0 first:ml-0 w-full rounded-none flex',
           active: {
-            on: 'p-4 text-gray-900 bg-gray-100 active dark:bg-gray-700 dark:text-white rounded-none',
-            off: 'bg-white hover:text-gray-700 hover:bg-gray-50 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700 rounded-none',
+            on: 'p-4 text-primary-100 font-bold bg-themeGray-70 active dark:bg-gray-700 dark:text-white rounded-none',
+            off: 'bg-white hover:text-themeGray-200 hover:bg-themeGray-60 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700 rounded-none',
           },
         },
       },
