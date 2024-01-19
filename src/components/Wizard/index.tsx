ecbf358a88967ccc582f55bea37d4ebb1c84c26b
Bhagya-Peramuna <bhagya@qbitum.com>
2024-01-19 14:45:41 +0530

    fix: linting issue resolved

--- src/components/Wizard/index.tsx
+++ src/components/Wizard/index.tsx
@@ -1,10 +1,10 @@
 export { Wizard } from './Wizard';
 export { WizardStep } from './WizardStep';
 export { HeaderBasic, HeaderProgress, HeaderTab } from './HeaderStyles';
-export { FooterButton, FooterNav } from './footerNav';
+export { FooterButton, FooterNav } from './FooterNav';
 
 export type { HeaderBasicProps, HeaderProgressProps, HeaderTabProps } from './HeaderStyles';
-export type { FooterButtonProps, FooterNavProps } from './footerNav';
+export type { FooterButtonProps, FooterNavProps } from './FooterNav';
 export type { WizardProps, FlowbiteWizardRootTheme, FlowbiteWizardTheme } from './Wizard';
 
 export type { WizardStepProps } from './WizardStep';
