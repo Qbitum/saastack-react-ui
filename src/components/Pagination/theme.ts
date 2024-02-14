3db6e0952b858aee93e30f99a158436d9ee1e294
Bhagya-Peramuna <bhagya@qbitum.com>
2024-02-14 17:02:04 +0530

    fix: table, pagination colour

--- src/components/Pagination/theme.ts
+++ src/components/Pagination/theme.ts
@@ -4,25 +4,25 @@ export const paginationTheme: FlowbitePaginationTheme = {
   base: '',
   layout: {
     table: {
-      base: 'text-sm text-themeGray-200 dark:text-gray-400',
-      span: 'font-semibold text-themeGray-200 dark:text-white',
+      base: 'text-sm text-gray-200 dark:text-gray-400',
+      span: 'font-semibold text-gray-200 dark:text-white',
     },
   },
   pages: {
     base: 'xs:mt-0 mt-2 inline-flex items-center -space-x-px',
     showIcon: 'inline-flex',
     previous: {
-      base: 'ml-0 rounded-l-lg border border-themeGray-50 bg-white py-1.5 px-3 leading-tight text-themeGray-200 enabled:hover:bg-themeGray-60 enabled:hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 enabled:dark:hover:bg-gray-700 enabled:dark:hover:text-white',
+      base: 'ml-0 rounded-l-lg border border-gray-400 bg-white py-1.5 px-3 leading-tight text-gray-800 enabled:hover:bg-gray-100 enabled:hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 enabled:dark:hover:bg-gray-700 enabled:dark:hover:text-white',
       icon: 'h-5 w-5',
     },
     next: {
-      base: 'rounded-r-lg border bborder-themeGray-50 bg-white py-1.5 px-3 leading-tight text-themeGray-100 enabled:hover:bg-themeGray-60 enabled:hover:text-themeGray-200 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 enabled:dark:hover:bg-gray-700 enabled:dark:hover:text-white',
+      base: 'rounded-r-lg border border-gray-400 bg-white py-1.5 px-3 leading-tight text-gray-800 enabled:hover:bg-blue-100 enabled:hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 enabled:dark:hover:bg-gray-700 enabled:dark:hover:text-white',
       icon: 'h-5 w-5',
     },
     selector: {
-      base: 'w-12 border border-themeGray-50 bg-white py-1.5 leading-tight text-themeGray-200 enabled:hover:bg-themeGray-60 enabled:hover:text-themeGray-200 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 enabled:dark:hover:bg-red-700 enabled:dark:hover:text-white',
+      base: 'w-12 border border-gray-400 bg-white py-1.5 leading-tight text-gray-700 enabled:hover:bg-blue-100 enabled:hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 enabled:dark:hover:bg-red-700 enabled:dark:hover:text-white',
       active:
-        'bg-themeGray-60 text-themeGray-200 font-bold hover:bg-themeGray-60 hover:text-themeGray-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white',
+        'bg-blue-100 text-gray-700 font-bold hover:bg-blue-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white',
       disabled: 'opacity-50 cursor-normal',
     },
   },
--- src/components/Table/theme.ts
+++ src/components/Table/theme.ts
@@ -2,25 +2,25 @@ import type { FlowbiteTableTheme } from './Table';
 
 export const tableTheme: FlowbiteTableTheme = {
   root: {
-    base: 'w-full text-left text-sm text-themeGray-100 dark:text-gray-400',
+    base: 'w-full text-left text-sm text-gray-800 dark:text-gray-400',
     shadow: 'absolute bg-white dark:bg-black w-full h-full top-0 left-0 rounded-lg drop-shadow-md -z-10',
     wrapper: 'relative',
   },
   body: {
     base: 'group/body',
     cell: {
-      base: 'group-first/body:group-first/row:first:rounded-tl-lg group-first/body:group-first/row:last:rounded-tr-lg group-last/body:group-last/row:first:rounded-bl-lg group-last/body:group-last/row:last:rounded-br-lg px-6 py-3 max-w-2 max-h-2',
+      base: 'group-first/body:group-first/row:first:rounded-tl-lg group-first/body:group-first/row:last:rounded-tr-lg group-last/body:group-last/row:first:rounded-bl-lg group-last/body:group-last/row:last:rounded-br-lg px-6 py-2 max-w-2 max-h-2',
     },
   },
   head: {
-    base: 'group/head text-xs uppercase text-themeGray-100 dark:text-gray-400 border-themeGray-70 p-0.5 border-y ',
+    base: 'group/head text-xs uppercase text-gray-800 dark:text-gray-400 border-gray-400 p-0.5 border-y ',
     cell: {
-      base: 'group-first/head:first:rounded-tl-lg group-first/head:last:rounded-tr-lg dark:bg-gray-700 px-6 py-2 max-w-4 ',
+      base: 'group-first/head:first:rounded-tl-lg group-first/head:last:rounded-tr-lg dark:bg-gray-700 px-6 py-1 max-w-4 ',
     },
   },
   row: {
     base: 'group/row',
-    hovered: 'hover:bg-themeGray-60 dark:hover:bg-gray-600',
-    striped: 'odd:bg-white even:bg-themeGray-70 odd:dark:bg-gray-800 even:dark:bg-gray-700',
+    hovered: 'hover:bg-gray-60 dark:hover:bg-gray-600',
+    striped: 'odd:bg-white even:bg-gray-70 odd:dark:bg-gray-800 even:dark:bg-gray-700',
   },
 };
